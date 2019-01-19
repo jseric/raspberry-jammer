187cda27c9764c4d14fc2f5d724ef9e2846c964d
Josip Seric <josip.seric@icloud.com>
2019-01-19 18:05:41 +0100

    commit_006
    - Added react-router-dom to client-side
    - Added a couple of dummy components (header, footer, home & dashboard)

---
+++ server/client/src/components/App.js
@@ -0,0 +1,43 @@
+// File name: App.js
+// Description: Main (App) component
+// Date created: 19th January 2018
+// Author: Josip Seric
+
+// Import modules
+import React, { Component } 	  from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+// Components
+const Header 	  = () => <h1>Hello</h1>;
+const Footer 	  = () => <h1>Footer</h1>;
+const Home		  = () => <h1>Home</h1>;
+const Dashboard = () => <h1>Dashboard</h1>;
+
+// App component
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+
+						<Route exact path="/"
+	                 component ={ Home } />
+
+						<Route path="/Dashboard"
+									 component={ Dashboard } />
+									 
+						<Footer />
+					</div>
+
+				</BrowserRouter>
+
+      </div>
+    ); // return
+  } // render()
+} // class App extends Component
+
+// Export App component
+export default App;
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,15 @@
+// File name: App.js
+// Description: Root file for client-side
+// Date created: 19th January 2018
+// Author: Josip Seric
+
+// Import modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './components/App';
 
+// Render app
 ReactDOM.render(
 	<App />,
 	document.getElementById('root')
